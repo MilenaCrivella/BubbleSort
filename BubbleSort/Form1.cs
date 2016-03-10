89a6af21ed04ec874f12b218688b3f22396e9508
Fabieli helena <fabielihelena@gmail.com>
2016-03-10 11:13:16 -0300

    Adicionando Números

---
+++ BubbleSort/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BubbleSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int>NumbersList = new List<int>();
+        private void AddNumbers(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                int n = random.Next(1, 10);
+                NumbersList.Add(n);
+            }
+            
+            foreach (int l in NumbersList)
+            {
+                    Console.WriteLine(l.ToString());
+            }  
+        }
+
+    }
+}
